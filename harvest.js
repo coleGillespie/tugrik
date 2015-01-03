322730b612cf6b06d269bd9c5cbd305dda5b3afd
Cole Gillespie <mcg42387@gmail.com>
2015-01-03 15:40:08 -0500

    update times into db

--- harvest.js
+++ harvest.js
@@ -1,9 +1,35 @@
-// Retrieve
-var mongo = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient,
+    bunyan = require('bunyan'),
+    log = bunyan.createLogger({
+      name: 'harvest'
+    });
+    request = require('request')
 
-// Connect to the db
-mongo.connect("mongodb://localhost:27017/db", function(err, db) {
-  if(!err) {
-    console.log("We are connected");
-  }
+var url = 'mongodb://localhost:27017/exampleDb';
+
+MongoClient.connect(url, function(err, db) {
+  log.info("Connected correctly to server");
+  
+  var collection = db.collection('test');
+
+  setInterval(function(){
+    request('http://localhost/nyt', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        log.info("checking nyt for new content")
+        JSON.parse(body).forEach(function(element, index, array){
+            collection.findOne({href:element.href}, function(err, item) {
+              if(item === null) {
+                log.info("cannot find ", element.href)
+                element.firstSeen = new Date().getTime();
+                collection.insert(element, {w:1}, function(err, result) {
+                  log.info("adding", result)
+                });
+              }
+            });
+        });
+      }
+    })
+
+  }, 300000);
+
 });
